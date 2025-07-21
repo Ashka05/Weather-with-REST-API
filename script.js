e38944fc674129161323e43e6688fee150e911df
Ashka05 <Ashka4542@gmail.com>
2025-07-21 10:48:43 +0530

    script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='435e43999bf706ea44c09b05cf2bba8e2';
+const API_KEY ='your-api-key';
 
 setInterval(() => {
     const time = new Date();
